bcc7cce46ea7c268b0eba4e7a9160caa837c61f7
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-10-15 20:14:17 -0700

    added fonts

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Heading from './components/heading'
 import Body from './components/body'
 import Process from './components/process'
+import Footer from './components/footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Heading/>
       <Body/>
       <Process/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+
+
+
+const Footer = props => (
+
+ <div>
+
+<FontAwesomeIcon icon="facebook" />
+
+
+ </div>
+)
+export default Footer
