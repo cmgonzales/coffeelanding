76597915981b68cc60a251dcd2d1b5c262b32fa7
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-12-08 17:34:08 -0800

    Updates

---
+++ precache-manifest.d8eff77c51c4a93651ae7fb444cbc18c.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbd95a1597f1c0d9bf9c6603f2e6af4e",
+    "url": "/coffeelanding/index.html"
+  },
+  {
+    "revision": "23718230ac78c7c87a7e",
+    "url": "/coffeelanding/static/css/2.fff34fbd.chunk.css"
+  },
+  {
+    "revision": "f996cd76cef2c9dc7384",
+    "url": "/coffeelanding/static/css/main.65569311.chunk.css"
+  },
+  {
+    "revision": "23718230ac78c7c87a7e",
+    "url": "/coffeelanding/static/js/2.26583a76.chunk.js"
+  },
+  {
+    "revision": "f996cd76cef2c9dc7384",
+    "url": "/coffeelanding/static/js/main.967513ea.chunk.js"
+  },
+  {
+    "revision": "42afc2a85ac246e2d371",
+    "url": "/coffeelanding/static/js/runtime-main.40bcae35.js"
+  },
+  {
+    "revision": "47c1e87687b64ca93fdc1d96b2ed782e",
+    "url": "/coffeelanding/static/media/beans.47c1e876.jpg"
+  },
+  {
+    "revision": "f551e7391170f3e70da9b1443f340390",
+    "url": "/coffeelanding/static/media/coffee.f551e739.jpg"
+  },
+  {
+    "revision": "dbe56f0db089264642a625bba3b7c5c2",
+    "url": "/coffeelanding/static/media/coffeeMobile.dbe56f0d.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/coffeelanding/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/coffeelanding/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/coffeelanding/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/coffeelanding/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/coffeelanding/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "21d5bda8eb5879b100cfe98664aebc90",
+    "url": "/coffeelanding/static/media/package.21d5bda8.jpg"
+  },
+  {
+    "revision": "09ce470242c91309f67559698bc2ad4c",
+    "url": "/coffeelanding/static/media/roasted.09ce4702.jpg"
+  },
+  {
+    "revision": "1f9df34ae6fe19440ccf0b9253f1205f",
+    "url": "/coffeelanding/static/media/steaming-coffee-beans.1f9df34a.jpg"
+  }
+]);
