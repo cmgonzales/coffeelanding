a0420e1cafd6d2a8a83e39ca71b289ba4bee33cf
Christopher Gonzales <cm.gonzales@yahoo.com>
2019-12-08 17:24:22 -0800

    production

---
+++ build/precache-manifest.e72d194465456bc5535ce70ed07b2a25.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b3d6f473d5300592a8da2d2af33a60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be80a0b7c0d6c9ee5b47",
+    "url": "/static/css/2.ab5dcbdc.chunk.css"
+  },
+  {
+    "revision": "18618e35009d50933cc0",
+    "url": "/static/css/main.65569311.chunk.css"
+  },
+  {
+    "revision": "be80a0b7c0d6c9ee5b47",
+    "url": "/static/js/2.26583a76.chunk.js"
+  },
+  {
+    "revision": "18618e35009d50933cc0",
+    "url": "/static/js/main.e9ce370e.chunk.js"
+  },
+  {
+    "revision": "262ea7edc450f594219b",
+    "url": "/static/js/runtime-main.e7f0abef.js"
+  },
+  {
+    "revision": "47c1e87687b64ca93fdc1d96b2ed782e",
+    "url": "/static/media/beans.47c1e876.jpg"
+  },
+  {
+    "revision": "f551e7391170f3e70da9b1443f340390",
+    "url": "/static/media/coffee.f551e739.jpg"
+  },
+  {
+    "revision": "dbe56f0db089264642a625bba3b7c5c2",
+    "url": "/static/media/coffeeMobile.dbe56f0d.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "21d5bda8eb5879b100cfe98664aebc90",
+    "url": "/static/media/package.21d5bda8.jpg"
+  },
+  {
+    "revision": "09ce470242c91309f67559698bc2ad4c",
+    "url": "/static/media/roasted.09ce4702.jpg"
+  },
+  {
+    "revision": "1f9df34ae6fe19440ccf0b9253f1205f",
+    "url": "/static/media/steaming-coffee-beans.1f9df34a.jpg"
+  }
+]);
