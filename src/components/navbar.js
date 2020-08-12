deb7d0e855eafe3653040029366fadd92eb843bf
Christopher Gonzales <cm.gonzales@yahoo.com>
2020-08-11 20:49:14 -0700

    added smooth scroll

---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function navbar() {
+    return (
+        <div>
+        <ul>
+            <li>About US</li>
+            <li>Our Coffee</li>
+            <li>The Process</li>
+            <li>Contact</li>
+        </ul>
+        </div>
+    )
+}
+
