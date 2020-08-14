58d1556c9b449a74285b01ebebb0b37215a3d5db
Christopher Gonzales <cm.gonzales@yahoo.com>
2020-08-13 22:32:51 -0700

    changed navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,15 +1,87 @@
-import React from 'react'
+import React, { Component } from "react";
+import logo from "../logo.svg";
+import { Link, animateScroll as scroll } from "react-scroll";
 
-export default function navbar() {
+export default class Navbar extends Component {
+  scrollToTop = () => {
+    scroll.scrollToTop();
+  };
+
+  render() {
     return (
-        <div>
-        <ul>
-            <li>About US</li>
-            <li>Our Coffee</li>
-            <li>The Process</li>
-            <li>Contact</li>
-        </ul>
+      <nav className="nav" id="navbar">
+        <div className="nav-content">
+          <img
+            src={logo}
+            className="nav-logo"
+            alt="Logo"
+            onClick={this.scrollToTop}
+          />
+          <ul className="nav-items">
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section1"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Section 1
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section2"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Section 2
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section3"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Section 3
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section4"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Section 4
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                activeClass="active"
+                to="section5"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+              >
+                Section 5
+              </Link>
+            </li>
+          </ul>
         </div>
-    )
+      </nav>
+    );
+  }
 }
 
