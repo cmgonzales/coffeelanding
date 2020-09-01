1f9569cfcddb3154ac2410c61f81b180855b2434
Christopher Gonzales <cm.gonzales@yahoo.com>
2020-08-31 21:43:09 -0700

    revamp nav bar

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,14 +2,7 @@ import React, { Component } from "react";
 import { Link, animateScroll as scroll } from "react-scroll";
 import '../css/nav.css'
 import logo from '../img/bean.png'
-import {
-  Nav,
-  NavItem,
-  Navbar,
-  NavLink,
-  NavbarBrand
-  
-} from 'reactstrap'
+
 export default class Navigation extends Component {
   scrollToTop = () => {
     scroll.scrollToTop();
@@ -17,62 +10,78 @@ export default class Navigation extends Component {
 
   render() {
     return (
-      <Navbar color="light" light expand="md">
-      <NavbarBrand >
-          <img
-            style = {{height: '25px'}}
-            src = {logo}
-            className="nav-logo"
-            alt="Logo"
-            onClick={this.scrollToTop}
-          />
-          </NavbarBrand>
-          <Nav className="mr-auto" navbar>
-          <NavItem>
-          <NavLink href = '#'>
-              <Link
-                activeClass="active"
-                to="section1"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={300}
-              >
-                About
-              </Link>
-              </NavLink>
-              </NavItem>
-           
-           <NavItem>
-           <NavLink href = '#'>
-              <Link
-                activeClass="active"
-                to="about"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Process
-              </Link>
-              </NavLink>
-              </NavItem>
-            <NavItem>
-            <NavLink href = '#'>
-              <Link
-                activeClass="active"
-                to="section3"
-                spy={true}
-                smooth={true}
-                offset={-70}
-                duration={500}
-              >
-                Contact US
-              </Link>
-              </NavLink>
-              </NavItem>
-         </Nav> 
-      </Navbar>
+      <nav className="nav" id="navbar">
+      <div className="nav-content">
+        <img
+          src={logo}
+          className="nav-logo"
+          alt="Logo"
+          onClick={this.scrollToTop}
+        />
+        <ul className="nav-items">
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to="section1"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Section 1
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to="section2"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Section 2
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to="section3"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Section 3
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to="section4"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Section 4
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              activeClass="active"
+              to="section5"
+              spy={true}
+              smooth={true}
+              offset={-70}
+              duration={500}
+            >
+              Section 5
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
     );
   }
 }
