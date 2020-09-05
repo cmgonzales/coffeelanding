5643facd8d1d80a2b3c6aaac1af8e0c1df0ae9db
Christopher Gonzales <cm.gonzales@yahoo.com>
2020-09-04 19:48:03 -0700

    changed layout for heading

--- src/components/intro.js
+++ src/components/intro.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { PropTypes } from "prop-types";
-import { Container, Row, Col } from "reactstrap";
+
 import { Button } from 'reactstrap';
 //import useModal from './useModal';
 import '../css/heading.css'
@@ -10,19 +10,14 @@ import '../css/heading.css'
 const Intro = props => {
  
   return(
-    <div>
-  <Container>
-    <Row>
-      <Col md="6"></Col>
-      <Col md="6">
+    <div clasName = "intro">
+  
         <div className = "coffee">
           <h1 className = "justCoffee">{props.title}</h1>
           <h3 className = "life">{props.description}</h3>
           <Button color="primary">Get a quote</Button>{' '}
           </div>
-      </Col>
-    </Row>
-  </Container> 
+     
   
   </div>
   )
